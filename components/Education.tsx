291d069ea2dbe99abf38eacfb22b0647faca3b75
Isabella <108432535+isabella82828@users.noreply.github.com>
2026-01-09 16:49:40 -0500

    Education

--- components/Education.tsx
+++ components/Education.tsx
@@ -5,23 +5,14 @@ const Education = (): JSX.Element => {
     return (
         <section className="grid" id="education">
             <h2 className="text-xl font-bold mt-12 mb-4">Education</h2>
-
-            <p className="text-base ml-4 text-gray-700">
-                Biomedical Engineering, Bachelor of Applied Science (BASc), University of Waterloo
-                <span className="float-right">2022–2027</span>
+            <p className = "text-base ml-4 text-gray-700">
+                Biomedical Engineering, Bachelors of Applied Science (BASc), University of Waterloo    &nbsp; &nbsp; &nbsp; &nbsp;   &nbsp;  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;  &nbsp;&nbsp;&nbsp;&nbsp;             2022-2027
+               
             </p>
-
-            <div className="ml-4 mt-3">
-                <p className="text-base font-semibold text-gray-800">Extracurriculars</p>
-                <ul className="list-disc list-inside text-gray-700 mt-1 space-y-1">
-                    <li>WATOLINK Brain Computer Interface Design Team</li>
-                    <li>Campus Response Team</li>
-                    <li>Intramural Soccer and Flag Football</li>
-                    <li>Stem Cell Club</li>
-                </ul>
-            </div>
         </section>
     );
 };
+
+
 
 export default Education;
